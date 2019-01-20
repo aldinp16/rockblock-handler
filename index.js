9549fa7eaa64c818bc288de6435a359b176b1bb1
Aldi Nugraha <aldinugrahap16@gmail.com>
2019-01-21 02:06:38 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.ROCKBLOCK_HANDLE_PORT
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
